a29d84f7a9e4e8fd7be32540f3cbf46702e04a70
jtotty <jt.totty1991@gmail.com>
2023-01-10 10:51:15 +0000

    Linear Search function

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,15 @@
+/**
+ * Linear search algorithm.
+ * Checks if a number is in an array of numbers.
+ *
+ * Linear time complexity: O(N)
+ */
 export default function linear_search(haystack: number[], needle: number): boolean {
+    for (let i = 0; i < haystack.length; ++i) {
+        if (haystack[i] === needle) {
+            return true;
+        }
+    }
 
+    return false;
 }
